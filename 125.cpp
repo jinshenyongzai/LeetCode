062beed54ae8675ae8614533319d1ace9d0933b3
FD-LJS <1021866202@qq.com>
2019-07-24 10:51:48 +0800

    Add files via upload

---
+++ 125.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPalindrome(string s) {
+
+		int left = 0, right = s.length() - 1;
+
+		while (left < right) {
+			
+			while (left < right && !isalnum(s[left]))
+				left++;
+			while (left < right && !isalnum(s[right]))
+				right--;
+
+			if (tolower(s[left]) != tolower(s[right]))
+				return false;
+
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+};
+
+//int main() {
+//
+//	string s = "A man, a plan, a canal: Panama";
+//
+//	bool ans = Solution().isPalindrome(s);
+//	cout <<  ans << endl;
+//}
