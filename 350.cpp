960d446d4df30d843f0adc0a4f9a1626d719b0d6
FD-LJS <1021866202@qq.com>
2019-07-25 16:18:03 +0800

    Update 350.cpp

--- 350.cpp
+++ 350.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <map>
+//#include <map>
+#include <unordered_map>
 
 using namespace std;
+
+//class Solution {
+//public:
+//	vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//
+//		map<int, int> record;
+//		for (int i = 0; i < nums1.size(); i++)
+//			record[nums1[i]]++;
+//
+//		vector<int> res;
+//
+//		for (int i = 0; i < nums2.size(); i++) {
+//			if (record[nums2[i]] > 0) {
+//				res.push_back(nums2[i]);
+//				record[nums2[i]]--;
+//			}
+//		}
+//
+//		return res;
+//	}
+//};
 
 class Solution {
 public:
