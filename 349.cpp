36ba511a2fd249c7dc0404222409cdaf4e4b9bae
FD-LJS <1021866202@qq.com>
2019-07-25 14:43:40 +0800

    Add files via upload

---
+++ 349.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+		set<int> record(nums1.begin(), nums1.end());
+
+		set<int> res;
+
+		for (int i = 0; i < nums2.size(); i++)
+			if (record.find(nums2[i]) != record.end())
+				res.insert(nums2[i]);
+
+		return vector<int>(res.begin(), res.end());
+	}
+};
+
+//int main() {
+//
+//	vector<int> nums1{ 1, 2, 2, 1 };
+//	vector<int> nums2{ 2, 2};
+//
+//	vector<int> ans = Solution().intersection(nums1, nums2);
+//	
+//	for (int i = 0; i < ans.size(); i++)
+//		cout << ans[i] << endl;
+//}
