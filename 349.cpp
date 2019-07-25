5d85f634b80ab76d22e7384ed684a8e6548f0b2c
FD-LJS <1021866202@qq.com>
2019-07-25 16:15:57 +0800

    Update 349.cpp

--- 349.cpp
+++ 349.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <set>
+//#include <set>
+#include <unordered_set>
 
 using namespace std;
+
+//class Solution {
+//public:
+//	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//
+//		set<int> record(nums1.begin(), nums1.end());
+//
+//		set<int> res;
+//
+//		for (int i = 0; i < nums2.size(); i++)
+//			if (record.find(nums2[i]) != record.end())
+//				res.insert(nums2[i]);
+//
+//		return vector<int>(res.begin(), res.end());
+//	}
+//};
 
 class Solution {
 public:
