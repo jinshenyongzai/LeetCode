37f036c98ec1a71d9dfdb21e8291044cf612d4a4
FD-LJS <1021866202@qq.com>
2019-07-30 13:46:20 +0800

    Add files via upload

--- 206.cpp
+++ 206.cpp
@@ -1,35 +1,84 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
- //Definition for singly-linked list.
  struct ListNode {
       int val;
       ListNode *next;
       ListNode(int x) : val(x), next(NULL) {}
  };
- 
-class Solution {
-public:
-	ListNode* removeElements(ListNode* head, int val) {
 
-	}
-};
+ void print_list(ListNode *head) {
+	 while (head->next != nullptr) {
+		 cout << head->val << "->";
+		 head = head->next;
+	 }
+	 cout << head->val << endl;
+ }
 
-//int main() {
-//
-//	ListNode* listNode;
-//	int value = 6;
-//
-//	int arr[] = { 1, 2, 6, 3 ,4, 5 ,6 };
-//
-//	for (int i = 0; i < size(arr); i++) {
-//		listNode = *listNode.next;
-//		listNode.val = arr[i];
-//	}
-//
-//	while (listNode.next != NULL) {
-//		cout << listNode.val << "<" << listNode.next << endl;
-//		listNode = *listNode.next;
-//	}
-//}
+ class List {
+ public:
+
+	 ListNode *head, *tail;
+	 int position;
+
+ public:
+	 List() {head = tail = nullptr;};
+
+	 List(const vector<int> &in) {
+		 head = tail = nullptr;
+		 for (auto it : in)
+			 this->insert(it);
+	 };
+
+	 ~List() { delete head, tail; };
+
+	 void insert(int x) {
+		 if (head == nullptr) {
+			 head = tail = new ListNode(x);
+			 head->next = tail->next = nullptr;
+		 }
+		 else {
+			 ListNode *p = new ListNode(x);
+			 tail->next = p;
+			 tail = p;
+			 tail->next = nullptr;
+		 }
+	 };
+
+	 void print() {
+		 while (head->next != nullptr) {
+			 cout << head->val << "->";
+			 head = head->next;
+		 }
+		 cout << head->val << endl;
+	 }
+ };
+
+ class Solution {
+ public:
+	 ListNode* reverseList(ListNode* head) {
+
+		 ListNode* pre = NULL;
+		 ListNode* cur = head;
+		 while (cur != NULL) {
+			 ListNode* next = cur->next;
+
+			 cur->next = pre;
+			 pre = cur;
+			 cur = next;
+		 }
+		 return pre;
+	 }
+ };
+
+int main() {
+
+	vector<int> nums = { 1, 2, 3, 4, 5 };
+	List list = List(nums);
+
+	print_list(list.head);
+	ListNode* head = Solution().reverseList(list.head);
+	print_list(head);
+}
