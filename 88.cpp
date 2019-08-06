5dce79dfffa43f00444a7cd0c27861d204b1d118
FD-LJS <1021866202@qq.com>
2019-08-06 18:29:21 +0800

    Update 88.cpp

--- 88.cpp
+++ 88.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -7,17 +6,30 @@ using namespace std;
 class Solution {
 public:
 	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		for (int i = 0; i < n; i++)
-			nums1[m + i] = nums2[i];
-		sort(nums1.begin(), nums1.end());
+		
+		int k = m + n - 1;
+		int i = m -1, j = n - 1;
+
+		while (i >= 0 && j >= 0) {
+			if (nums1[i] > nums2[j])
+				nums1[k--] = nums1[i--];
+			else
+				nums1[k--] = nums2[j--];
+		}
+
+		while (i < 0 && k >= 0)
+			nums1[k--] = nums2[j--];
+
+		while (j < 0 && k > 0)
+			nums1[k--] = nums1[i--];
 	}
 };
 
 int main() {
 
-	vector<int> nums1{ 1, 2, 3, 0, 0, 0 };
-	vector<int> nums2{ 2, 5, 6 };
-	int m = 3, n = 3;
+	vector<int> nums1{ 0 };
+	vector<int> nums2{ 1 };
+	int m = 0, n = 1;
 
 	Solution().merge(nums1, m, nums2, n);
 
