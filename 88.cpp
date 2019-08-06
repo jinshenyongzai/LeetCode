9dae992794b6bb56ebe9db923b74575f282e1c6e
FD-LJS <1021866202@qq.com>
2019-08-06 16:23:58 +0800

    Update 88.cpp

--- 88.cpp
+++ 88.cpp
@@ -1,37 +1,28 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-	vector<int> sortArrayByParity(vector<int>& A) {
-
-		auto len = static_cast<int>(A.size());
-		int left = 0, right = len - 1;
-
-		while(left <= right){
-
-			if (A[left] % 2 == 0) {
-				left++;
-				continue;
-			}
-
-			if (A[right] % 2 != 0) {
-				right--;
-				continue;
-			}
-
-			swap(A[left++], A[right--]);
-		}
-		return A;
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		for (int i = 0; i < n; i++)
+			nums1[m + i] = nums2[i];
+		sort(nums1.begin(), nums1.end());
 	}
 };
 
-//int main() {
-//
-//	vector<int> A{ 3,1,2,4 };
-//
-//	Solution().sortArrayByParity(A);
-//}
+int main() {
+
+	vector<int> nums1{ 1, 2, 3, 0, 0, 0 };
+	vector<int> nums2{ 2, 5, 6 };
+	int m = 3, n = 3;
+
+	Solution().merge(nums1, m, nums2, n);
+
+	int i = 0;
+	for (i = 0; i < m + n - 1; i++)
+		cout << nums1[i] << ", ";
+	cout << nums1[i] << endl;
+}
