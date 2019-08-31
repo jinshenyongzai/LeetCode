157130e897f246dd3938e8b9ab3c4bdd5a1dec6c
FD-LJS <1021866202@qq.com>
2019-08-31 15:50:17 +0800

    Add files via upload

--- 136.cpp
+++ 136.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+//class Solution {
+//public:
+//	int singleNumber(vector<int>& nums) {
+//
+//		int res = 0;
+//		for (int i = 0; i < nums.size(); i++)
+//			res ^= nums[i];
+//			
+//		return res;
+//	}
+//};
 
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
 
+		map<int, int> visited;
+
 		int res = 0;
 		for (int i = 0; i < nums.size(); i++) {
-			res = res ^ nums[i];
-			cout << res << endl;
+
+			if (!visited[nums[i]]) {
+				res += nums[i];
+				visited[nums[i]] = 1;
+			}
+			else
+				res -= nums[i];
 		}
-			
+
 		return res;
 	}
 };
 
 int main() {
 
-	vector<int> nums1 = { 2, 2, 1 };
-	vector<int> nums2 = { 4, 1, 2, 1, 2 };
-
-	cout << Solution().singleNumber(nums1) << endl;
-	cout << Solution().singleNumber(nums2) << endl;
+	vector<int> nums = { 4, 1, 2, 1, 2 };
+	cout << Solution().singleNumber(nums) << endl;
 
 	return 0;
 }
