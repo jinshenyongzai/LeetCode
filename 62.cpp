a7ca2f64051164938a9b7314456fdafbaa9e0bfa
FD-LJS <1021866202@qq.com>
2019-09-02 09:12:30 +0800

    Add files via upload

---
+++ 62.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+
+		vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+		for (int i = 1; i <= m; i++)
+			dp[i][1] = 1;
+
+		for (int j = 1; j <= n; j++)
+			dp[1][j] = 1;
+
+		for (int i = 2; i <= m; i++)
+			for (int j = 2; j <= n; j++)
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+		return dp[m][n];
+	}
+};
+
+int main() {
+
+	int m = 3, n = 2;
+	cout << Solution().uniquePaths(m, n) << endl;
+	
+	return 0;
+}
