1f359fa4f6f77e722e5c03c3e9bdf5c237b0201d
FD-LJS <1021866202@qq.com>
2019-09-17 08:07:00 +0800

    Add files via upload

---
+++ 191.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		int count = 0;
+		for (int i = 0; i < 32; i++) {
+			if (n & 1)
+				count++;
+			n = n >> 1;
+		}
+		return count;
+	}
+};
+
+int main() {
+
+	uint32_t n = 0000101;
+	int ans = Solution().hammingWeight(n);
+	cout << ans << endl;
+}
