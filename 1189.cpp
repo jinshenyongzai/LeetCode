58154de6ebdbe884e7bca12e976739129a7c2e26
FD-LJS <1021866202@qq.com>
2019-09-18 10:16:25 +0800

    Add files via upload

---
+++ 1189.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxNumberOfBalloons(string text) {
+
+		unordered_map<char, int> cnt;
+		for (auto& ch : text)
+			cnt[ch]++;
+
+		return min({cnt['b'], cnt['a'], cnt['l'] / 2, cnt['o'] / 2, cnt['n']});
+	}
+};
+
+int main() {
+
+	string text = "nlaebolko";
+	int ans = Solution().maxNumberOfBalloons(text);
+	
+	cout << ans << endl;
+}
