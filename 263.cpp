ca464e8e6a637066b035a6140b23d3a76418d71a
FD-LJS <1021866202@qq.com>
2019-09-22 15:16:37 +0800

    Add files via upload

---
+++ 263.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isUgly(int num) {
+
+		for (int i = 2; i < 6 && num; i++)
+			if (i != 4)
+				while (num % i == 0)
+					num /= i;
+		return num == 1;
+	}
+};
+
+int main() {
+
+	int num = 2;
+
+	bool ans = Solution().isUgly(num);
+	cout << ans << endl;
+}
