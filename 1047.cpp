86309c94e419aeed80de0ca327b8ddac69115bb7
FD-LJS <1021866202@qq.com>
2019-09-24 23:35:08 +0800

    Add files via upload

---
+++ 1047.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+	string removeDuplicates(string S) {
+
+		stack<char> s;
+		int len = S.size();
+		for (int i = len - 1; i >= 0; i--) {
+			s.push(S[i]);
+			while (i - 1 >= 0 && s.size()) {
+				if (s.top() == S[i - 1]) {
+					s.pop();
+					i--;
+				}
+				else
+					break;
+			}
+		}
+
+		string str;
+		while (s.size()) {
+			str += s.top();
+			s.pop();
+		}
+
+		return str;
+	}
+};
+
+int main() {
+
+	string S = "abbaca";
+	string ans = Solution().removeDuplicates(S);
+	cout << ans << endl;
+}
