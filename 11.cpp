cc16ea3db15af909d950aef7da3316068f6cb9e0
FD-LJS <1021866202@qq.com>
2019-09-28 10:35:58 +0800

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -24,17 +24,17 @@ public:
 	}
 };
 
-//int main() {
-//
-//	vector<int> height1 = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-//	vector<int> height2 = { 2, 3, 10, 5, 7, 8, 9 };
-//	vector<int> height3 = { 1, 3, 2, 5, 25, 24, 5 };
-//
-//	int ans1 =  Solution().maxArea(height1);
-//	int ans2 = Solution().maxArea(height2);
-//	int ans3 = Solution().maxArea(height3);
-//
-//	cout << ans1 << endl;
-//	cout << ans2 << endl;
-//	cout << ans3 << endl;
-//}
+int main() {
+
+	vector<int> height1 = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+	vector<int> height2 = { 2, 3, 10, 5, 7, 8, 9 };
+	vector<int> height3 = { 1, 3, 2, 5, 25, 24, 5 };
+
+	int ans1 =  Solution().maxArea(height1);
+	int ans2 = Solution().maxArea(height2);
+	int ans3 = Solution().maxArea(height3);
+
+	cout << ans1 << endl;
+	cout << ans2 << endl;
+	cout << ans3 << endl;
+}
