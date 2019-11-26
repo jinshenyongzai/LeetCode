1f56335b1fa40313f8f744838d821f35e7580b85
FD-LJS <1021866202@qq.com>
2019-11-26 10:49:21 +0800

    Update 189.cpp

--- 189.cpp
+++ 189.cpp
@@ -28,9 +28,9 @@ public:
 	}
 };
 
-int main() {
+// int main() {
 
-	vector<int> nums{ 1,2,3,4,5,6,7 };
-	int k = 3;
-	Solution().rotate(nums, k);
-}
+// 	vector<int> nums{ 1,2,3,4,5,6,7 };
+// 	int k = 3;
+// 	Solution().rotate(nums, k);
+// }
